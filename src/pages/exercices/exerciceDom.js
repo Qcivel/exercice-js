9e7dafd52f23db7243751e79b02a4d50ac2ab588
Qcivel <q.civel@yahoo.fr>
2026-08-31 15:52:50 +0200

    Exercice DOM

---
+++ src/pages/exercices/exerciceDom.js
@@ -0,0 +1,8 @@
+let titre = document.querySelector("h1");
+let paragraphe = document.querySelector("p");
+console.log(titre);
+titre.textContent= "Bonjour";
+
+paragraphe.textContent = "Je suis un texte";
+
+paragraphe.style.color="red";
---
+++ src/pages/exercices/variable.js
@@ -0,0 +1,23 @@
+let myAge = 25;
+let texte = "Loick est un gros noob sur Akali !"
+let float = 1.20;
+let tab = [1,2,3,4,5];
+let classe = {
+    name: "Quentin",
+    age: 31
+}
+let helloWorld = ()=>{
+    return "Hello Word";
+}
+
+console.log(myAge);
+console.log(texte);
+console.log(float);
+console.log(tab);
+console.log(classe.name);
+console.log(hello());
+console.log(helloWorld());
+
+function hello(){
+    console.log("Hello Word");
+}
