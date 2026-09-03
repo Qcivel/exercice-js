10cad886ba1ba9ee487ac85ba87b5482525a5a27
Qcivel <q.civel@yahoo.fr>
2026-09-03 15:21:22 +0200

    exercice regex psw

--- src/pages/exercices/regex.js
+++ src/pages/exercices/regex.js
@@ -3,7 +3,8 @@ const charDecimal = /\d/;
 const charSpecial = /[$&@!]/;
 
 let mail = document.querySelector("#mail");
-
+let psw = document.querySelector("#psw");
+let error = document.querySelector("#error");
 mail.addEventListener("keyup",()=>{
     console.log(mail.value);
     if (regexMail.test(mail.value)){
@@ -14,4 +15,33 @@ mail.addEventListener("keyup",()=>{
 
 })
 console.log(regexMail.test(mail.value));
+let pswTooLong = "";
+let pswTooShort ="";
+let specialCharacter ="";
+let pswDecimal ="";
 
+psw.addEventListener("keyup",()=>{
+    console.log(psw.value.length);
+    if(psw.value.length <= 6){
+        pswTooLong = " Le mot de passe doit contenir au moins 6 caractères.";
+    }else{
+        pswTooLong ="";
+    } if(psw.value.length > 12){
+        pswTooShort = " Le mot de passe ne doit  pas contenir plus de 12 caractères.";
+    }else{
+        pswTooShort ="";
+    }
+    if (!charSpecial.test(psw.value)){
+         specialCharacter = " Il faut au moins 1 caractère spécial.";
+    }else{
+        specialCharacter = "";
+    }
+    if (!charDecimal.test(psw.value)){
+         pswDecimal = " Il faut au moins 1 chiffre.";
+    }else{
+        pswDecimal = "";
+    }
+
+
+    error.textContent = pswTooLong + pswTooShort + specialCharacter + pswDecimal;
+})
